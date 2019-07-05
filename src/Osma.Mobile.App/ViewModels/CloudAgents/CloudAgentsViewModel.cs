87937abb2099fa3601ab11eb6531c4cc0bb81cab
gargamul <agarg@palo-it.com>
2019-07-05 18:16:55 +0800

    Missing cloudAgent files

---
+++ src/Osma.Mobile.App/ViewModels/CloudAgents/CloudAgentsViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using AgentFramework.Core.Models.Wallets;
+using Osma.Mobile.App.Services.Interfaces;
+using Osma.Mobile.App.Services.Models; 
+using Osma.Mobile.App.Utilities;
+using Xamarin.Forms;
+using ReactiveUI;
+using AgentFramework.Core.Models.Records;
+using System.Threading.Tasks;
+using AgentFramework.Core.Contracts;
+using Autofac;
+
+namespace Osma.Mobile.App.ViewModels.CloudAgents
+{
+    public class CloudAgentsViewModel : ABaseViewModel
+    {
+        private readonly ICustomAgentContextProvider _agentContextProvider;
+        private readonly ICloudAgentRegistrationService _registrationService;
+        private readonly ILifetimeScope _scope;
+
+        public CloudAgentsViewModel(IUserDialogs userDialogs, 
+                                 INavigationService navigationService,
+                                 ICustomAgentContextProvider agentContextProvider,
+                                 ICloudAgentRegistrationService registrationService,
+                                 ILifetimeScope scope) : base(
+                                 nameof(CloudAgentsViewModel), 
+                                 userDialogs, 
+                                 navigationService)
+        {
+            _agentContextProvider = agentContextProvider;
+            _registrationService = registrationService;
+            _scope = scope;
+        }
+
+        public override async Task InitializeAsync(object navigationData)
+        {
+            await RefreshCloudAgents();
+            await base.InitializeAsync(navigationData);
+        }
+
+        public async Task RefreshCloudAgents()
+        {
+            RefreshingCloudAgents = true;
+
+            var context = await _agentContextProvider.GetContextAsync();
+
+            CloudAgentsGrouped = await _registrationService.GetAllCloudAgentAsync(context.Wallet);
+            RefreshingCloudAgents = false;
+        }
+
+        public async Task DeleteCloudAgent()
+        {
+
+        }
+
+        #region Bindable Command
+        public ICommand RefreshCommand => new Command(async () => await RefreshCloudAgents());
+
+        public ICommand DeleteCommand => new Command(async () => await DeleteCloudAgent());
+        
+        #endregion
+
+        #region Bindable Properties
+        private List<CloudAgentRegistrationRecord> _cloudAgentsGrouped;
+        public List<CloudAgentRegistrationRecord> CloudAgentsGrouped
+        {
+            get => _cloudAgentsGrouped;
+            set => this.RaiseAndSetIfChanged(ref _cloudAgentsGrouped, value);
+        }
+
+        private bool _refreshingCloudAgents;
+        public bool RefreshingCloudAgents
+        {
+            get => _refreshingCloudAgents;
+            set => this.RaiseAndSetIfChanged(ref _refreshingCloudAgents, value);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Osma.Mobile.App/Views/CloudAgents/CloudAgentsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Osma.Mobile.App.Views.CloudAgents
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CloudAgentsPage : ContentPage
+    {
+        public CloudAgentsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
